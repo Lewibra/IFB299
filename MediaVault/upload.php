3cba9297a1d43611ee2e516c640cb6cb4d00acfb
Lewis <lewistracy321@hotmail.com>
2017-06-07 13:34:49 +1000

    Revert "fixes"
    This reverts commit 25083c4fcd68a06d01cf3ad46247114fe91efa4b.

--- MediaVault/upload.php
+++ MediaVault/upload.php
@@ -9,14 +9,16 @@ if(!empty($_FILES)){
     $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
     $fileSize = filesize($fileName);
     $fileId = uniqid($userName);
+    $string = preg_replace('/\s+/', '', $targetFile);
+    $newName = preg_replace('/\s+/', '', $fileName);
 
     if (file_exists($_FILES['file']['tmp_name'])){
-        if(move_uploaded_file($_FILES['file']['tmp_name'], $targetFile)){
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $string)){
             //insert file information into db table
             //Please note that the table is configured to auto increment
             //Register the new folder into the SQL database
             $sql = "INSERT INTO `Media_Vault_Schema`.`file_details` (`user_name`, `file_name`, `file_type`, `file_location`, `location_inside`, `details`)
-                    VALUES ('".$_SESSION['login_user']."', '".$fileName."','".$fileType."', '".$_SESSION['location']."', '".$_SESSION['location']."/".$fileName."', '".$fileName."')";
+                    VALUES ('".$_SESSION['login_user']."', '".$newName."','".$fileType."', '".$_SESSION['location']."', '".$_SESSION['location']."/".$newName."', '".$newName."')";
 
             if ($conn->query($sql) === FALSE) {
                 echo "Error: " . $sql . "<br>" . $conn->error;
